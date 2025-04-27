c3ffbd94e0a349fbefa94c1c43f32986e707083c
junhaoyu <yu1791046157@gmail.com>
2025-04-27 23:43:39 +0800

    NIM

--- train.py
+++ train.py
@@ -28,6 +28,7 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
 
 from model import GPTConfig, GPT
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 # -----------------------------------------------------------------------------
 # default config values designed to train a gpt2 (124M) on OpenWebText
